04f2224c24d893ccfc704fa24464d206a3357f21
Reuben Morais <reuben.morais@gmail.com>
2024-12-31 10:36:57 +0100

    Switch to k3d

--- clusters/home/bootstrap.sh
+++ clusters/home/bootstrap.sh
@@ -1,2 +1,5 @@
+# create k3d cluster
+k3d cluster create --config k3d.config.yaml
+
 # set GITHUB_TOKEN env var
 flux bootstrap github --token-auth --owner=reuben --repository=home-cluster --branch=main --path=clusters/home --personal
